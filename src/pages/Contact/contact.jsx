7f974cef8644c52022ea7f1913f1fbbe5c81d55e
LeslieLobry <leslielobry@gmail.com>
2024-04-09 20:22:52 +0200

    contact

--- src/pages/Contact/contact.jsx
+++ src/pages/Contact/contact.jsx
@@ -1,8 +1,16 @@
-
+import Form from "../../components/Form/form"
+import "../Contact/contact.css"
 
 function Contact(){
-    return(
-        <div></div>
-    )
+return(
+<div className="contact-contenant">
+    <div className="contenant">
+      <div className="contact-title">
+        <h2>Contact</h2>
+    </div>
+    <Form />
+    </div>
+</div>
+)
 }
 export default Contact
